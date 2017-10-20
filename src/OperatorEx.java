0fb3b0d7324db74d66d672665e700437d21de27b
khi <khi805@naver.com>
2017-10-20 11:31:39 +0900

    first

---
+++ src/OperatorEx.java
@@ -0,0 +1,46 @@
+import java.lang.*;
+
+public class OperatorEx {
+	public static void main(String[] args) {
+		//산술 연산자
+		System.out.println(5 + 3);
+		System.out.println(5 - 3);
+		System.out.println(5 * 3);
+		System.out.println(5 / 3);
+		System.out.println(5 % 3);
+		
+		//단항 연산자
+		int i = 1;
+		i++;
+		System.out.println(i);
+		i--;
+		System.out.println(i);
+		
+		boolean  b = true;
+		System.out.println(b);
+		System.out.println(!b);
+		
+		//관계 연산자
+		System.out.println(3 == 3); // 같다 참
+		System.out.println(3 != 3); // 다르면 참
+		System.out.println(3 > 3); // lv가 크면 참
+		System.out.println(3 >= 3); // lv가 크거나 같으면 참
+		System.out.println(3 < 3); // rv가 크면 참
+		System.out.println(3 <= 3); // rv가 크거나 같으면 참
+		
+		// 조건 연산자 (논리연산자)
+		System.out.println("조건연산자 &&");
+		System.out.println(true && true); // 양변이 참이여야 참
+		System.out.println(false && false); 
+		System.out.println(true && false); // 하나라도 거짓이면 거짓
+		System.out.println(false && true);
+		
+		System.out.println("조건연산자 ||");
+		System.out.println(true || true); // 하나만 참이면 참
+		System.out.println(false || false); // 거짓 
+		System.out.println(true || false); // 참
+		System.out.println(false || true); // 참
+		
+		
+	}
+}
---
+++ src/exam/MyFirstClass.java
@@ -0,0 +1,16 @@
+package exam;
+
+public class MyFirstClass {
+	public static void main(String args[]) {
+		// 데이터 타입
+		int i = 1; //4byte
+		short s = 1; //2byte
+		long l = 1L; //8byte
+		byte b = 1; //1byte
+		float f = 1F; //4byte
+		double d = 1; //8byte
+		char c = 'a'; //1byte
+		boolean bo= true; //1byte
+		System.out.println(123344);
+	}
+}
